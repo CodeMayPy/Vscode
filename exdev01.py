2dddc29c792db416223685232b2bd485bdfcbbdf
Mayara <code.maypy@gmail.com>
2024-12-19 15:10:04 -0300

    updated

---
+++ exdev01.py
@@ -0,0 +1,10 @@
+#Crie um programa para praticar o uso do print() junto com f-string. Imprima, formatando, os seguintes dados:
+#10 em decimal;
+#10 de decimal para binário
+#3.14159265 com todas as casas decimais
+#3.14159265 com duas casas decimais
+
+print(f'O valor inteiro em decimal é:{10: d}')
+print(f'O valor em binário é:{10: b}')
+print(f' O valor de PI é: {3.14159265: f}')
+print(f'O valor de Pi com duas casas decimais é: {3.14159265:.2f}')
